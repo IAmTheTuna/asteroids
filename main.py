975404eda23c00ed89f45da84fae6d98320b0517
IAmTheTuna <amatunas@me.com>
2025-02-12 10:32:18 -0700

    updating main to print info

--- main.py
+++ main.py
@@ -2,9 +2,14 @@
 # the open-source pygame library
 # throughout this file
 import pygame
+from constants import *
 
 def main():
-    return print("Starting asteroids!")
+    start_message = print("Starting asteroids!")
+    screen_width = print(f'Screen width: {SCREEN_WIDTH}')
+    screen_height = print(f"Screen height: {SCREEN_HEIGHT}")
+    return start_message, screen_height, screen_width
+
 
 if __name__ == "__main__":
     main()
