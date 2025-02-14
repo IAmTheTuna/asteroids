7cfa2c59ca43c80601105238c2a941c0aef9664b
IAmTheTuna <amatunas@me.com>
2025-02-14 08:49:51 -0700

    added the ability to rotate a player on the screen

--- player.py
+++ player.py
@@ -6,10 +6,10 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-
+    # defining the draw method in order to draw the player on the screen
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
-
+    # defining the player shape as a triangle
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -17,3 +17,15 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    # adding ability to rotate player
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+                
+        if keys[pygame.K_d]:
+            self.rotate(dt)
